5296e29107195397c6b7acdad4ff285185b53365
kiyohito-luk <kiyohito.luk@gmail.com>
2024-03-06 22:25:16 +0900

    小修正03062225

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
